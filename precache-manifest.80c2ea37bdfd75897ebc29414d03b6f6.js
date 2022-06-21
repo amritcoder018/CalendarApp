f9d439907fb3ca82eb25ada759a5ece933baad92
amritgit <amritkr.sharma018@gmail.com>
2022-06-21 16:21:21 +0530

    Updates

---
+++ precache-manifest.80c2ea37bdfd75897ebc29414d03b6f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8c5691076df6a7fb44e",
+    "url": "/CalendarApp/static/css/main.ecfc16ec.chunk.css"
+  },
+  {
+    "revision": "f8c5691076df6a7fb44e",
+    "url": "/CalendarApp/static/js/main.f8c56910.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/CalendarApp/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "b38ce12b724441baa895",
+    "url": "/CalendarApp/static/js/2.b38ce12b.chunk.js"
+  },
+  {
+    "revision": "cb49e4fe10def1828ff3",
+    "url": "/CalendarApp/static/js/runtime~main.cb49e4fe.js"
+  },
+  {
+    "revision": "db08ccde7ebd995412956e50f5b37bdc",
+    "url": "/CalendarApp/index.html"
+  }
+];
