51cba5652709f6c22fbcaf08d3e40c0e7293f5eb
amritgit <amritkr.sharma018@gmail.com>
2022-06-21 14:50:04 +0530

    calender app first commit

---
+++ src/Check2.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Check2()
+{
+    return <h1>Check2sfsdfsdfsdfsdf</h1>;
+}
+export default Check2;
---
+++ src/check.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Check(prop)
+{
+    return <h1>dance sdfsdfsfs {prop.love}</h1>;
+}
+export default Check;
